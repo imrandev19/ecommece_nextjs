7f994e5bcb3c041368d672595c8ddcd256dc5fd2
Md. Imran Hossain <imrandev19@gmail.com>
2025-06-04 23:28:16 +0600

    container done

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+
+import React from "react";
+import Container from "./Container";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="header_add bg-[#191C1F]">
+      
+       <Container>
+        <Image src="/images/headerAdd.png" width={160} height={43} alt="offer image"/>
+        </Container> 
+    
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
