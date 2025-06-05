7bb9b02038e0695684702b4cd17fd4860e603ddb
Md. Imran Hossain <imrandev19@gmail.com>
2025-06-05 20:28:36 +0600

    welcome part done

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,6 +5,12 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import { FaArrowRightLong } from "react-icons/fa6";
 import { IoIosClose } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaPinterestP } from "react-icons/fa";
+import { FaReddit } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Header = () => {
   return (
     <header>
@@ -17,8 +23,35 @@ const Header = () => {
         <Button className="text-[#191C1F] font-bold text-sm leading-[48px] tracking-wide bg-[#EBC80C] hover:bg-[#EBC80C]/90 " >SHOP NOW <FaArrowRightLong /></Button>
        </div>
         </Container> 
-          <Button className="p-2 bg-[#303639] hover:bg-[#303639]/90 absolute top-2/4 translate-y-[-50%] right-2"><IoIosClose className="text-white" /></Button>
+          <Button className="p-2 bg-[#303639] hover:bg-[#303639]/90 absolute top-2/4 translate-y-[-50%] right-2"><IoIosClose className="text-white w-[16px] h-[16px]" /></Button>
       </div>
+      {/* Header Ads part End */}
+
+      {/* Header Welcome Part & Search Part Start */}
+      <div className="header_blue bg-[#1B6392] ">
+        <div className="header_welcome py-4 ">
+          <Container>
+            <div className="flex items-center justify-between">
+              <h3 className="text-[#FFFFFF] text-sm leading-5 font-normal">Welcome to Clicon online eCommerce store. </h3>
+            <div className="flex items-center gap-3 text-white ">
+              <p>Follow us:</p>
+              <FaTwitter className="w-4 h-4 text-white" />
+              <FaFacebook className="w-4 h-4 text-white"  />
+              <FaPinterestP className="w-4 h-4 text-white"  />
+              <FaReddit className="w-4 h-4 text-white" />
+              <FaYoutube className="w-4 h-4 text-white" />
+              <FaInstagram className="w-4 h-4 text-white" />
+              <div className="w-[.5px] h-[28px] ml-3 bg-white">
+                
+                </div>
+            </div>
+            </div>
+          </Container>
+        </div>
+        <hr/>
+        <div className="header_search w-full h-10 "></div>
+      </div>
+
     </header>
   );
 };
